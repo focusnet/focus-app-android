09ce858adfdabea688afa901a607e70a5e8c7a79
Julien Künzi <julien.kuenzi@bfh.ch>
2016-02-14 10:09:13 +0700

    refactor constants

--- app/src/main/java/eu/focusnet/app/ui/util/Constant.java
+++ app/src/main/java/eu/focusnet/app/ui/util/Constant.java
@@ -6,24 +6,25 @@ package eu.focusnet.app.ui.util;
 public class Constant
 {
 	// Extra parameters names
-	public static final String UI_EXTRA_PROJECT_PATH = "ProjectPath",
-			UI_EXTRA_PAGE_PATH = "PagePath",
-			UI_EXTRA_TITLE = "Title",
-			UI_EXTRA_PATH = "Path",
-			UI_EXTRA_IS_TO_SAVE = "IsToSave",
-			UI_EXTRA_NAME = "Name",
-			UI_EXTRA_BOOKMARK_TYPE = "BookmarkType",
-			UI_EXTRA_NOTIFICATION_ID = "NotificationId",
-			UI_EXTRA_DELAY_INTERVAL = "DelayInterval";
-	
-	// and bundle
-	public static final String UI_BUNDLE_FRAGMENT_TITLE = "FragmentTitle",
-			UI_BUNDLE_FRAGMENT_POSITION = "FragmentPosition";
+	public static final String UI_EXTRA_PROJECT_PATH = "eu.focusnet.extra.PROJECT_PATH",
+			UI_EXTRA_PAGE_PATH = "eu.focusnet.extra.PAGE_PATH",
+			UI_EXTRA_TITLE = "eu.focusnet.extra.TITLE",
+			UI_EXTRA_PATH = "eu.focusnet.extra.PATH",
+			UI_EXTRA_IS_TO_SAVE = "eu.focusnet.extra.IS_TO_SAVE",
+			UI_EXTRA_NAME = "eu.focusnet.extra.NAME",
+			UI_EXTRA_BOOKMARK_TYPE = "eu.focusnet.extra.BOOKMARK_TYPE",
+			UI_EXTRA_NOTIFICATION_ID = "eu.focusnet.extra.NOTIFICATION_ID",
+			UI_EXTRA_DELAY_INTERVAL = "eu.focusnet.extra.DELAY_INTERVAL";
 
-					// fragments identifiers
-	public static final int UI_FRAGMENT_FOCUS = 1, UI_FRAGMENT_BOOKMARK = 2,
-					UI_FRAGMENT_SYNCHRONIZE = 3,
-					UI_FRAGMENT_SETTINGS = 4,
-					UI_FRAGMENT_USER_MANUAL = 5;
+	// and bundle
+	public static final String UI_BUNDLE_FRAGMENT_TITLE = "eu.focusnet.bundle.FRAGMENT_TITLE",
+			UI_BUNDLE_FRAGMENT_POSITION = "eu.focusnet.bundle.FRAGMENT_POSITION";
+
+	// fragments identifiers
+	public static final int UI_FRAGMENT_FOCUS = 1,
+			UI_FRAGMENT_BOOKMARK = 2,
+			UI_FRAGMENT_SYNCHRONIZE = 3,
+			UI_FRAGMENT_SETTINGS = 4,
+			UI_FRAGMENT_USER_MANUAL = 5;
 
 }
